3812c8ffa99c2ef987751ef401f3e9bf6d6d3aef
smit thakkar <smitthakkar96@gmail.com>
2017-07-25 15:49:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     license="BSD",
     keywords="Asciibinder, ascii_binder_search",
     url="https://github.com/smitthakkar96/ascii_binder_search_plugin",
-    install_requires=['lxml', 'pyyaml', 'xmltodict', 'beautifulsoup4'],
+    install_requires=['lxml', 'pyyaml', 'xmltodict', 'beautifulsoup4', 'xmltodict'],
     py_modules=['ascii_binder_search'],
     include_package_data=True,
     zip_safe=False,
