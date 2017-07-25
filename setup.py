7505706829853dc8f179bdd666eab6a95c30bc31
smit thakkar <smitthakkar96@gmail.com>
2017-07-25 18:21:11 +0530

    Fixed module structure

--- setup.py
+++ setup.py
@@ -8,16 +8,14 @@ setup(
     author="smit thakkar",
     author_email="smitthakkar96@gmail.com",
     description=("A small utility that would help you to get search in asciibinder"),
-    package_data={'static': '*'},
+    packages=['ascii_binder_search'],
     license="BSD",
     keywords="Asciibinder, ascii_binder_search",
     url="https://github.com/smitthakkar96/ascii_binder_search_plugin",
     install_requires=['lxml', 'pyyaml', 'xmltodict', 'beautifulsoup4', 'xmltodict'],
-    py_modules=['ascii_binder_search'],
-    data_files=['static/search.html'],
     include_package_data=True,
     zip_safe=False,
     scripts=[
-        'ascii_binder_search'
+        'ascii_binder_search/ascii_binder_search'
     ]
 )
